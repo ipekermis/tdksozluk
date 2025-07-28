abb140cbb8f53cccbc8e85aaf8599af570fa4c3d
ipekermis <zeynepermis999@gmail.com>
2025-07-28 10:18:40 +0300

    feat: adding and removing words to FavoriteWords list

--- lib/utilities/local_storage_helper.dart
+++ lib/utilities/local_storage_helper.dart
@@ -1,17 +1,20 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-class LocalStorageHelper{
-  static final LocalStorageHelper _instance=LocalStorageHelper._();
-  static  SharedPreferences? _preferences;
+class LocalStorageHelper {
+  static final LocalStorageHelper _instance = LocalStorageHelper._();
+  static SharedPreferences? _preferences;
+
   LocalStorageHelper._();
-  factory LocalStorageHelper({SharedPreferences? pref}){
-    _preferences ??=pref;
+
+  factory LocalStorageHelper({SharedPreferences? pref}) {
+    _preferences ??= pref;
     return _instance;
   }
-  Future<void> setString(String key,String value) async{
-    await _preferences?.setString(key, value);
 
+  Future<void> setString(String key, String value) async {
+    await _preferences?.setString(key, value);
   }
+
   Future<void> setStringList(String key, List<String> value) async {
     await _preferences?.setStringList(key, value);
   }
@@ -23,21 +26,37 @@ class LocalStorageHelper{
   Future<void> remove(String key) async {
     await _preferences?.remove(key);
   }
+
   static const String _keyInstance = 'key';
+  static const String _favoriteKeyInstance = 'favorite_key';
   static const int _maxWords = 10;
 
   List<String> getLastSearchedWords() {
     return getStringList(_keyInstance) ?? [];
   }
-  Future<void> addSearchedWord(String word) async {
+
+  List<String> getFavoriteWords() {
+    return getStringList(_favoriteKeyInstance) ?? [];
+  }
+
+  Future<void> addToFavWord(String word) async {
     if (_preferences == null) return;
 
-    List<String> currentList =
-        getLastSearchedWords();
+    List<String> currentList = getFavoriteWords();
 
     currentList.remove(word);
     currentList.insert(0, word);
 
+    await setStringList(_favoriteKeyInstance, currentList);
+  }
+
+  Future<void> addSearchedWord(String word) async {
+    if (_preferences == null) return;
+
+    List<String> currentList = getLastSearchedWords();
+
+    currentList.remove(word);
+    currentList.insert(0, word);
 
     if (currentList.length > _maxWords) {
       currentList = currentList.sublist(0, _maxWords);
@@ -49,9 +68,16 @@ class LocalStorageHelper{
   Future<void> clearSearchedWords() async {
     await remove(_keyInstance);
   }
+
   Future<void> removeWord(String word) async {
     final words = getLastSearchedWords();
     words.remove(word);
     await setStringList(_keyInstance, words);
+  }
+
+  Future<void> removeFavWord(String word) async {
+    final words = getFavoriteWords();
+    words.remove(word);
+    await setStringList(_favoriteKeyInstance, words);
   }
 }
